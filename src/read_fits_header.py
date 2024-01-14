53963bb655a2a39e7a2bb05303bee96007993576
victorbgh16 <victor.berghausen@gmail.com>
2024-01-14 15:30:02 +0100

    added program to read fits header

--- src/read_fits_header.py
+++ src/read_fits_header.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
 
-with fits.open('../data/Io2400.fit') as hdulist:
+with fits.open('data/Io2400nr2.fit') as hdulist:
      hdulist.info()
      for hdu in hdulist:
          print(repr(hdu.header))
